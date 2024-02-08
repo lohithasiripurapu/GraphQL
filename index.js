3058214eb61cf99ecea23ba4131d4a0369899359
lohithasiripurapu <lohitha.siripurapu@gmail.com>
2024-02-07 19:05:03 -0600

    GraphQL first query

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString
+} = require('graphql');
+
+const app = express();
+
+const schema = new GraphQLSchema({ 
+    query: new GraphQLObjectType({
+        name: 'HelloWorld',
+        fields: () => ({
+            message: { 
+                type: GraphQLString,
+                resolve: () => 'Hello World...........'
+            }
+        })
+    })
+});
+
+// function expressGraphQLMiddleware(options) {
+//     return graphqlHTTP(options);
+// }
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}));
+
+const port = 5000;
+app.listen(port, () => console.log('listening on port ' + port));
